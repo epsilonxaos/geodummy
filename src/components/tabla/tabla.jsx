109c4b734322ccb7eec49c48fe1680deeb61c412
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-01-12 12:56:23 -0600

    Cambios en iconos y dropdowns

--- src/components/tabla/tabla.jsx
+++ src/components/tabla/tabla.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { getColorBaged } from '../../assets/js/helpers';
 import useResults from '../../hooks/useResults';
 import { filterContain } from "../../assets/js/helpers"
-import {MdOutlineMoreVert, MdAutorenew, MdWarning, MdDeleteForever} from 'react-icons/md';
+import {MdMoreHoriz, MdAutorenew, MdWarning, MdDeleteForever, MdCheckCircle, MdDangerous} from 'react-icons/md';
 import Dropdown from '../dropdown/dropdown';
 
 import {Marker, VectorLayer} from 'maptalks';
@@ -93,33 +93,21 @@ const Tabla = ({titles, data, mapa, getIdSelect}) => {
 export default Tabla;
 
 const TrList = ({addMarker, id, estado, tipo, referencia, etapa, location, active}) => {
-	const iconActivo = <svg className="icon icon-tabler icon-tabler-circle-check" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00b341" fill="none" strokeLinecap="round" strokeLinejoin="round">
-			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-			<circle cx="12" cy="12" r="9" />
-			<path d="M9 12l2 2l4 -4" />
-		</svg>;
-
-	const iconInactivo = <svg className="icon icon-tabler icon-tabler-circle-x" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fd0061" fill="none" strokeLinecap="round" strokeLinejoin="round">
-			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-			<circle cx="12" cy="12" r="9" />
-			<path d="M10 10l4 4m0 -4l-4 4" />
-		</svg>;
-
 	return(
 		<tr className={` border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${id == active ? 'bg-gray-100 dark:bg-gray-600' : 'bg-white'}`}>
-			<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+			<th scope="row" className="cursor-pointer px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" onClick={() => addMarker(location, id)}>
 				{id}
 			</th>
-			<td className="px-6 py-4">
-				{estado === 'activo' ? iconActivo : iconInactivo}
+			<td className="cursor-pointer px-6 py-4" onClick={() => addMarker(location, id)}>
+				{estado === 'activo' ? <MdCheckCircle color={"#00b341"} size={"20px"} /> : <MdDangerous color={"#fd0061"} size={"20px"}/>}
 			</td>
-			<td className="px-6 py-4">
+			<td className="cursor-pointer px-6 py-4" onClick={() => addMarker(location, id)}>
 				{tipo}
 			</td>
-			<td className="px-6 py-4" onClick={(ev) => {ev.stopPropagation(); addMarker(location, id);}}>
-				<span className="font-semibold hover:text-sky-600 cursor-pointer">{referencia}</span>
+			<td className="cursor-pointer px-6 py-4" onClick={() => addMarker(location, id)}>
+				<span className="font-semibold">{referencia}</span>
 			</td>
-			<td className="px-6 py-4">
+			<td className="cursor-pointer px-6 py-4" onClick={() => addMarker(location, id)}>
 				{moment().format('L')}
 			</td>
 			<td className="px-6 py-4">
@@ -130,15 +118,20 @@ const TrList = ({addMarker, id, estado, tipo, referencia, etapa, location, activ
 			<td className="px-6 py-4">
 				<Dropdown>
 					<Dropdown.Button>
-						<MdOutlineMoreVert size={"20px"} />
+						<MdMoreHoriz size={"20px"} />
 					</Dropdown.Button>
 					<Dropdown.Body>
 						<ul className="text-sm text-gray-700 dark:text-gray-200">
 							<li>
-								<span className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><MdAutorenew className="inline-block mr-1" size={"18px"} /> Cambiar estado</span>
+								{
+									estado === 'activo' ? 
+										<span className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white group"><MdDangerous className="inline-block mr-1 group-hover:text-[#fd0061]" size={"18px"} /> Suspender</span>
+									:
+										<span className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white group"><MdCheckCircle className="inline-block mr-1 group-hover:text-[#00b341]" size={"18px"} /> Activar</span>
+								}
 							</li>
 							<li>
-								<span className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><MdWarning className="inline-block mr-1" size={"18px"} /> Reportar</span>
+								<span className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white group"><MdWarning className="inline-block mr-1 group-hover:text-yellow-400" size={"18px"} /> Reportar</span>
 							</li>
 							<li>
 								<span className="cursor-pointer block px-4 py-2 hover:bg-red-700 hover:text-white dark:hover:bg-gray-600 dark:hover:text-white"><MdDeleteForever className="inline-block mr-1" size={"18px"} /> Eliminar</span>
