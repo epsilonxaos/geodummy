d44cdcfc0d2da7358fdde94486c5090ceab8c077
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-01-12 15:50:52 -0600

    Estilos

--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -9,11 +9,11 @@ const TabInfo = ({tabs}) => {
 				{
 					tabs && (
 						<div className="relative overflow-x-auto">
-							<table className="w-full text-sm text-left text-gray-500 dark:text-gray-200">
+							<table className="w-full text-sm text-left text-gray-800 dark:text-gray-200">
 								<tbody>
 									<tr className="">
 										<th scope="row" className="w-28 px-6 pr-1 py-4 font-semibold text-gray-900 dark:text-gray-200 whitespace-nowrap"> ID: </th>
-										<td className="px-6 py-4 text-gray-800"> {tabs.id} </td>
+										<td className="px-6 py-4"> {tabs.id} </td>
 									</tr>
 									<tr className="">
 										<th scope="row" className="w-28 px-6 pr-1 py-4 font-semibold text-gray-900 dark:text-gray-200 whitespace-nowrap"> Estado: </th>
@@ -21,11 +21,11 @@ const TabInfo = ({tabs}) => {
 									</tr>
 									<tr className="">
 										<th scope="row" className="w-28 px-6 pr-1 py-4 font-semibold text-gray-900 dark:text-gray-200 whitespace-nowrap"> Tipo: </th>
-										<td className="px-6 py-4 text-gray-800"> {tabs.tipo} </td>
+										<td className="px-6 py-4"> {tabs.tipo} </td>
 									</tr>
 									<tr className="">
 										<th scope="row" className="w-28 px-6 pr-1 py-4 font-semibold text-gray-900 dark:text-gray-200 whitespace-nowrap"> Referencia: </th>
-										<td className="px-6 py-4 text-gray-800"> {tabs.referencia} </td>
+										<td className="px-6 py-4"> {tabs.referencia} </td>
 									</tr>
 									<tr className="">
 										<th scope="row" className="w-28 px-6 pr-1 py-4 font-semibold text-gray-900 dark:text-gray-200 whitespace-nowrap"> Etapa: </th>
