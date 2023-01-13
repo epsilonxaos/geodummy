7e5f09ac584136b59f2b54d4a21489230fd9335a
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-01-13 10:00:11 -0600

    Reduccion de marrgin bottom en titulo

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function App() {
 				<Sidebar toggleTheme={toggleTheme} />
 
 				<div className="p-3 pt-5">
-					<h3 className="flex items-center mb-8 text-gray-700 dark:text-white border-b border-b-gray-100 dark:border-b-gray-500 pb-2">
+					<h3 className="flex items-center mb-3 text-gray-700 dark:text-white border-b border-b-gray-100 dark:border-b-gray-500 pb-2">
 						<MdAccountTree size={"2.25rem"} /> <span className="font-semibold text-4xl uppercase pl-4">Proyectos</span>
 					</h3>
 					<div className="grid-cols-4 grid gap-2">
