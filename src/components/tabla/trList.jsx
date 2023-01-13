9e3513c377647a524621e53d9357eb8016f4516c
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-01-13 10:32:01 -0600

    Padding en y a 0.5

--- src/components/tabla/trList.jsx
+++ src/components/tabla/trList.jsx
@@ -6,27 +6,27 @@ import Dropdown from '../dropdown/dropdown';
 const TrList = ({addMarker, id, estado, tipo, referencia, etapa, location, active}) => {
 	return(
 		<tr className={` border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${id == active ? 'bg-gray-100 dark:bg-gray-600' : 'bg-white'}`}>
-			<th scope="row" className="cursor-pointer px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white" onClick={() => addMarker(location, id)}>
+			<th scope="row" className="cursor-pointer px-6 py-0.5 font-medium text-gray-900 whitespace-nowrap dark:text-white" onClick={() => addMarker(location, id)}>
 				{id}
 			</th>
-			<td className="cursor-pointer px-6 py-1" onClick={() => addMarker(location, id)}>
+			<td className="cursor-pointer px-6 py-0.5" onClick={() => addMarker(location, id)}>
 				{estado === 'activo' ? <MdCheckCircle color={"#00b341"} size={"20px"} /> : <MdDangerous color={"#fd0061"} size={"20px"}/>}
 			</td>
-			<td className="cursor-pointer px-6 py-1" onClick={() => addMarker(location, id)}>
+			<td className="cursor-pointer px-6 py-0.5" onClick={() => addMarker(location, id)}>
 				{tipo}
 			</td>
-			<td className="cursor-pointer px-6 py-1" onClick={() => addMarker(location, id)}>
+			<td className="cursor-pointer px-6 py-0.5" onClick={() => addMarker(location, id)}>
 				<span className="font-semibold">{referencia}</span>
 			</td>
-			<td className="cursor-pointer px-6 py-1" onClick={() => addMarker(location, id)}>
+			<td className="cursor-pointer px-6 py-0.5" onClick={() => addMarker(location, id)}>
 				{moment().format('L')}
 			</td>
-			<td className="px-6 py-1 dark:text-gray-800">
+			<td className="px-6 py-0.5 dark:text-gray-800">
 				<span className={`${getColorBaged(etapa)} text-xs font-medium mr-2 px-2.5 py-0.5 rounded `}>
 					{etapa}
 				</span>
 			</td>
-			<td className="px-6 py-1 w-10">
+			<td className="px-6 py-0.5 w-10">
 				<Dropdown>
 					<Dropdown.Button>
 						<MdMoreVert size={"20px"} />
