aa6deb8a552a94f8d3c311253b096b73a0ca1eb6
González Ramón Jesus <jesus.gonzalez@municipio.merida.gob.mx>
2023-01-13 11:21:02 -0600

    Titles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,31 +20,31 @@ const Sidebar = ({toggleTheme}) => {
 		<>
 			<aside className={`${!active ? "w-[50px]" : "w-[200px]"} group  ${active && ("sidebar-is-active")} bg-white dark:bg-gray-800 transition-all h-full fixed top-0 left-0 z-10 border-r dark:border-r-slate-800`} aria-label="Sidebar" style={{boxShadow: "0 2px 2px 0 rgb(0, 0, 0, 0.2)"}} ref={ref}>
 				<div className="dark:bg-gray-800">
-					<div className="bg-[#254088] dark:bg-black px-4 py-3.5">
-						<div className="flex items-center justify-center group-[.sidebar-is-active]:justify-start text-white">
-							<FiMenu size={"24px"} className="cursor-pointer" onClick={() => handlerToggleActive()} />
+					<div className="bg-[#254088] dark:bg-black">
+						<div className="flex items-center justify-center group-[.sidebar-is-active]:justify-start text-white px-4 py-3.5">
+							<FiMenu size={"24px"} className="cursor-pointer" onClick={() => handlerToggleActive()} title="menu" />
 							<p className="font-semibold hidden group-[.sidebar-is-active]:block text-lg pl-5">GEOPORTAL</p>
 						</div>
 
-						<div className="hidden group-[.sidebar-is-active]:flex items-center justify-between pt-6 text-white">
+						<div className="hidden group-[.sidebar-is-active]:flex items-center justify-between px-4 py-3.5 text-white cursor-pointer">
 							<p>Anónimo</p>
-							<RxCaretDown />
+							<RxCaretDown title="Menu usuario"/>
 						</div>
 					</div>
 					<ul className="text-[#6e7a81] dark:text-gray-200">
-						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4 bg-sky-100 dark:bg-gray-700">
+						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4 bg-sky-100 dark:bg-gray-700" title="Proyectos">
 							<MdAccountTree size={"18px"} /> <span className="hidden group-[.sidebar-is-active]:block pl-3 font-semibold text-sm uppercase">Proyectos</span>
 						</li>
-						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4">
+						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4" title="Mapa">
 							<MdOutlineMap size={"18px"} /> <span className="hidden group-[.sidebar-is-active]:block pl-3 font-semibold text-sm uppercase">Mapa</span>
 						</li>
-						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4 border-pink-500 border-[1px] bg-[#ec008c1f]">
+						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4 border-pink-500 border-[1px] bg-[#ec008c1f]" title="Ayuda">
 							<MdHelpOutline size={"18px"} /> <span className="hidden group-[.sidebar-is-active]:block pl-3 font-semibold text-sm uppercase">Ayuda</span>
 						</li>
-						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4">
+						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4" title="Compartir">
 							<MdShare size={"18px"} /> <span className="hidden group-[.sidebar-is-active]:block pl-3 font-semibold text-sm uppercase">Compartir</span>
 						</li>
-						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4" onClick={() => {toggleTheme()}}>
+						<li className="py-3.5 flex justify-center items-center cursor-pointer group-[.sidebar-is-active]:justify-start px-4" onClick={() => {toggleTheme()}} title="Modo oscuro">
 							<MdOutlineLightMode className="hidden dark:block" /> <MdOutlineNightlight className="block dark:hidden" />
 							<span className="hidden group-[.sidebar-is-active]:block pl-3 font-semibold text-sm uppercase"><span className="block dark:hidden">Modo oscuro</span> <span className="hidden dark:block">Modo claro</span></span>
 						</li>
